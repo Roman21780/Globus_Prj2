a8fb9ab9b15064b3b90e755925f792be566f228c
Roman <Sapunovrm21780@gmail.com>
2025-11-06 23:22:05 +0500

    добавлена сущность User

---
+++ src/main/java/com/example/prj2/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.prj2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
