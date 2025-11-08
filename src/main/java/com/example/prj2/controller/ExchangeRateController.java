0951758275d345fe921f824d8e0865a0b12a58d4
Roman <Sapunovrm21780@gmail.com>
2025-11-09 00:19:13 +0500

    Добавлен ExchangeRateController.java

---
+++ src/main/java/com/example/prj2/controller/ExchangeRateController.java
@@ -0,0 +1,36 @@
+package com.example.prj2.controller;
+
+import com.example.prj2.service.ExchangeRateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rates")
+@RequiredArgsConstructor
+public class ExchangeRateController {
+
+    private final ExchangeRateService exchangeRateService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> getRates() {
+        return ResponseEntity.ok(exchangeRateService.getRates());
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateRates() {
+        exchangeRateService.updateRatesFromCBR();
+        return ResponseEntity.ok("Updated");
+    }
+
+    @PostMapping("/fake-update")
+    public ResponseEntity<String> fakeUpdate() {
+        exchangeRateService.fakeUpdateRates();
+        return ResponseEntity.ok("Fake updated");
+    }
+}
