878a8e349d254c2bcae522fdebf10c2e8597fef6
Roman <Sapunovrm21780@gmail.com>
2025-11-22 00:26:00 +0500

    добавлена сущность для курсов валют ExchangeRate.java

---
+++ src/main/java/com/example/prj2/entity/ExchangeRate.java
@@ -0,0 +1,42 @@
+package com.example.prj2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "exchange_rates")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExchangeRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false, precision = 10, scale = 4)
+    private BigDecimal rate;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
